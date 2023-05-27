c86841c56afdcec88eb6a5a66657d7854ff6816b
RMehdid <s.mahdid@univ-boumerdes.dz>
2023-05-27 19:25:09 +0100

    added navbar component

---
+++ iTheat/Components/NavBar.swift
@@ -0,0 +1,20 @@
+//
+//  NavBar.swift
+//  iTheat
+//
+//  Created by Samy Mehdid on 27/5/2023.
+//
+
+import SwiftUI
+
+struct NavBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavBar_Previews: PreviewProvider {
+    static var previews: some View {
+        NavBar()
+    }
+}
