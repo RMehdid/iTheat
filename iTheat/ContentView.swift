a8d41ac081397c22bb0ba8a2b04e7e44ec835635
RMehdid <s.mahdid@univ-boumerdes.dz>
2023-05-27 19:18:35 +0100

    Initial Commit

---
+++ iTheat/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iTheat
+//
+//  Created by Samy Mehdid on 27/5/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iTheat/iTheatApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTheatApp.swift
+//  iTheat
+//
+//  Created by Samy Mehdid on 27/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct iTheatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
